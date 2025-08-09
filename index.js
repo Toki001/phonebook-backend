2dda8d8075d09845d9b698507f6b3a5b5d3aec80
Jameskier Doliguez <jameskierdoliguez@Tokis-MacBook-Pro.local>
2025-08-09 20:04:02 +0800

    Updated with dist

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('body', (request) => {
     return request.method === 'POST' ? JSON.stringify(request.body) : ''
